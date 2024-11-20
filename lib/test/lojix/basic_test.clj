c82796e7cd429772b2589bbabe78e9507f8b5cbd
li <li@maisiliym.criome.me>
2024-11-20 13:38:02 -0600

    [added.negativeDerivationTest added.testMainOpts]

--- lib/test/lojix/basic_test.clj
+++ lib/test/lojix/basic_test.clj
@@ -1,6 +1,6 @@
 (ns lojix.basic-test
   (:require
-   [malli.core :as malli]
+   [malli.core :as m]
    [lojix.nix :as nix]
    [clojure.test :refer [are deftest is testing]]))
 
@@ -10,6 +10,18 @@
    :builder "/bin/sh"
    :args ["-c" "echo hello world > $out"]})
 
+(def bad-derivation
+  {:name "hello-world-derivation"
+   :system ["x86_64-linux" "aarch64-linux"]
+   :builder "/bin/sh"
+   :args ["-c" "echo bad derivation > $out"]})
+
 (deftest derivation-typecheck
   (testing "hello-world"
-    (is (malli/validate nix/Derivation hello-world-derivation))))
+    (is (m/validate nix/Derivation hello-world-derivation))))
+
+(deftest failing-tests
+  (testing "Tests that should fail"
+    (is (not (m/validate nix/Derivation bad-derivation)))))
+
+
