8d617e49cfc0810823ab327fc2894c9d68c5ddc5
li <li@maisiliym.criome.me>
2024-11-26 09:20:37 -0600

    savedWork

--- lib/test/lojix/defun_test.clj
+++ lib/test/lojix/defun_test.clj
@@ -5,13 +5,20 @@
    [malli.core :as m]
    [clojure.test :refer [deftest is testing]]))
 
-(defn times :- :int
+(defmulti times)
+
+(defn times-int :- :int
   "x times y"
   [x :- :int, y :- :int]
   (* x y))
+
+(deftest uninstrumented-typed-functions-tests
+  (testing "uninstrumented typed functions"
+    (is (= (times 1 "oops") 3))))
 
 (mi/instrument!)
 
 (deftest typed-functions-tests
   (testing "typed functions"
-    (is (= (times 1 3) 3))))
+    (is (= (times 1 3) 3))
+    (is (= (times 1 "oops") 3))))
